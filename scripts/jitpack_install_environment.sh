ca92171333a67c845390d541dcdb1fcebab5389a
Toby Bonhof <apps@2b.work>
2020-07-09 16:11:52 +0200

    jitpack

--- scripts/jitpack_install_environment.sh
+++ scripts/jitpack_install_environment.sh
@@ -4,7 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
-
 echo "==> Installing Yarn package manager"
 rm -rf ~/.yarn
 curl -o- -L https://yarnpkg.com/install.sh | bash
@@ -12,6 +11,4 @@ curl -o- -L https://yarnpkg.com/install.sh | bash
 export PATH="$HOME/.yarn/bin:$PATH"
 yarn config set prefix ~/.yarn -g
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-alias nodejs=node
-pwd
 yarn install 
